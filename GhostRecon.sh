f2fc58423a27ec38fefc6c7f9e57c2a0a47f763b
AashishSec <65489287+aashishsec@users.noreply.github.com>
2023-12-10 06:51:17 +0530

    Update GhostRecon.sh

--- GhostRecon.sh
+++ GhostRecon.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <domain>"
+    exit 1
+fi
+
+domain="$1"
+
+#web.archive.org 
+
+curl -s "http://web.archive.org/cdx/search/cdx?url=*.${domain}/*&output=text&fl=original&collapse=urlkey" | sed -e 's_https*://__' -e "s/\/.*//" | sort -u | tee web_archive.txt
+
+#crt.sh 
+
+curl "https://crt.sh/?q=${domain}" | grep -F ".${domain}" | sort -u | cut -d'>' -f2 | cut -d'<' -f1 | sort -u | tee crt.txt
+
+#UrlScan.io
+
+#AlienVault
+
 
