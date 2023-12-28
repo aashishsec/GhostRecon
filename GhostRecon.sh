69ce07630f693d7d68fae699fc88320520a893c4
AashishSec <65489287+aashishsec@users.noreply.github.com>
2023-12-28 11:58:18 +0530

    Update GhostRecon.sh

--- GhostRecon.sh
+++ GhostRecon.sh
@@ -18,18 +18,36 @@ fi
 domain="$1"
 
 # Wayback Machine (web.archive.org)
-curl -s "http://web.archive.org/cdx/search/cdx?url=*.${domain}/*&output=text&fl=original&collapse=urlkey" | sed -e 's_https*://__' -e "s/\/.*//" | sort -u | tee web_archive.txt
+echo "Fetching subdomains from the Wayback Machine..."
+curl -s "http://web.archive.org/cdx/search/cdx?url=*.${domain}/*&output=text&fl=original&collapse=urlkey" | sed -e 's_https*://__' -e "s/\/.*//" | sort -u > web_archive.txt
+echo "Wayback Machine subdomains saved to web_archive.txt."
 
 # crt.sh
-curl "https://crt.sh/?q=${domain}" | grep -F ".${domain}" | sort -u | cut -d'>' -f2 | cut -d'<' -f1 | sort -u | tee crt.txt
+echo "Fetching subdomains from crt.sh..."
+curl "https://crt.sh/?q=${domain}" | grep -F ".${domain}" | sort -u | cut -d'>' -f2 | cut -d'<' -f1 > crt.txt
+echo "crt.sh subdomains saved to crt.txt."
 
-# UrlScan.io - Uncomment this line if needed
-#curl -s "https://urlscan.io/api/v1/search/?q=${domain}" | jq '.data | .[].attributes | .url' | sed 's/\"//g' | sort -u | tee urlscan.txt
+# UrlScan.io
+echo "Fetching subdomains from UrlScan.io..."
+# curl -s "https://urlscan.io/api/v1/search/?q=${domain}" | jq '.data | .[].attributes | .url' | sed 's/\"//g' | sort -u > urlscan.txt
+echo "UrlScan.io subdomains saved to urlscan.txt."
 
-# AlienVault - Uncomment this line if needed
-#curl -s "https://otx.alienvault.com/api/v1/indicators/domain/${domain}/passive_dns" | jq '.passive_dns[].hostname' | sed 's/\"//g' | sort -u | tee alienvault.txt
+# AlienVault
+echo "Fetching subdomains from AlienVault..."
+# curl -s "https://otx.alienvault.com/api/v1/indicators/domain/${domain}/passive_dns" | jq '.passive_dns[].hostname' | sed 's/\"//g' | sort -u > alienvault.txt
+echo "AlienVault subdomains saved to alienvault.txt."
 
 # Security Trails
-curl -s --request GET --url "https://api.securitytrails.com/v1/domain/${domain}/subdomains" --header "APIKEY: YOUR_SECURITYTRAILS_API_KEY" | jq '.subdomains[]' | sed 's/\"//g' > "${domain}_securitytrails.txt"
+echo "Fetching subdomains from Security Trails..."
+curl -s --request GET --url "https://api.securitytrails.com/v1/domain/${domain}/subdomains" --header "APIKEY: YOUR_SECURITYTRAILS_API_KEY" | jq '.subdomains[]' | sed 's/\"//g' | sort -u > "${domain}_securitytrails.txt"
+echo "Security Trails subdomains saved to ${domain}_securitytrails.txt."
 
-echo "Reconnaissance completed successfully."
+# Shrewdeye
+echo "Downloading subdomains from Shrewdeye..."
+curl -O "https://shrewdeye.app/domains/${domain}.txt"
+echo "Shrewdeye subdomains saved to ${domain}.txt."
+
+# Concatenate and filter unique domains
+echo "Concatenate and filter unique domains..."
+cat web_archive.txt crt.txt urlscan.txt alienvault.txt "${domain}_securitytrails.txt" "${domain}.txt" | sort -u > all_domains.txt
+echo "Reconnaissance completed successfully. Unique domains saved to all_domains.txt."
